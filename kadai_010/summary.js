8d28dd2188148016a20c8fd06811d22645b67d07
nagisa713 <163708254+nagisa713@users.noreply.github.com>
2024-06-02 17:52:47 +0900

    kadai_010wo

---
+++ kadai_010/summary.js
@@ -0,0 +1,18 @@
+$(function(){
+  //ボタンがクリックされたら文字色変化
+  $('#change-color').on('click',function(){
+    $('#target').css('color','red');
+  });
+  //ボタンがクリックされたら文字内容変化
+  $('#change-text').on('click',function(){
+    $('#target').text('Hello!');
+  });
+  //ボタンがクリックされたらフェードアウトで文字が消える
+  $('#fade-out').on('click',function(){
+    $('#target').fadeOut();
+  });
+  //ボタンがクリックされたらフェードインで文字が現れる
+  $('#fade-in').on('click',function(){
+    $('#target').fadeIn();
+  });
+});
